268688f14a27fe7f955967b5939cca791a764c7e
Huy <hinhthaihuy@gmail.com>
2025-06-06 00:30:41 +0700

    Edit,DeleteAccident

--- SurTraff_BE/src/main/java/com/example/demo/repository/AccidentRepository.java
+++ SurTraff_BE/src/main/java/com/example/demo/repository/AccidentRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 @Repository
 public interface AccidentRepository extends JpaRepository<Accident, Long> {
     List<Accident> findByCameraId(Long cameraId);
+
+    @Override
+    List<Accident> findAll();
 } 
