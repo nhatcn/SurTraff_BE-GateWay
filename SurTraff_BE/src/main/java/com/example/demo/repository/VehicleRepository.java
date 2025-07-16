17c5de411b35d4054b200b00f81c5cd303e20589
AnhTus-dey <108685458+AnhTus-dey@users.noreply.github.com>
2025-07-16 19:58:37 +0700

    Update VehicleRepository.java

--- SurTraff_BE/src/main/java/com/example/demo/repository/VehicleRepository.java
+++ SurTraff_BE/src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -4,7 +4,10 @@ import com.example.demo.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByUserId(Long userId);
+
+    Optional<Vehicle> findByLicensePlate(String licensePlate);
 }
