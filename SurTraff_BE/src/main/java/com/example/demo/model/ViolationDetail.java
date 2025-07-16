66f480add7ddf4e6dfd8f5343c886ae268b019f9
Anh Tu Nguyen <tunace172085@gmail.com>
2025-07-16 19:33:27 +0700

    update violations

--- SurTraff_BE/src/main/java/com/example/demo/model/ViolationDetail.java
+++ SurTraff_BE/src/main/java/com/example/demo/model/ViolationDetail.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class ViolationDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "violation_id", nullable = false)
--- SurTraff_BE/src/main/java/com/example/demo/repository/ViolationRepository.java
+++ SurTraff_BE/src/main/java/com/example/demo/repository/ViolationRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Violation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface ViolationRepository extends JpaRepository<Violation, Long> {
     List<Violation> findByVehicleLicensePlateOrderByCreatedAtDesc(String licensePlate);
+
+    @Query("SELECT v FROM Violation v LEFT JOIN FETCH v.vehicle LEFT JOIN FETCH v.camera LEFT JOIN FETCH v.vehicleType LEFT JOIN FETCH v.violationDetails WHERE 1=1")
+    List<Violation> findAll();
 }
