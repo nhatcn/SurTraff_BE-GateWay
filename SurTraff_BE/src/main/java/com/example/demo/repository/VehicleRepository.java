6de42bf7e3c64123f61dfafad5a177af7b63db92
Anh Tu Nguyen <tunace172085@gmail.com>
2025-07-16 19:36:03 +0700

    update violations

--- SurTraff_BE/src/main/java/com/example/demo/repository/VehicleRepository.java
+++ SurTraff_BE/src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,16 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.User;
 import com.example.demo.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     Optional<Vehicle> findByLicensePlate(String licensePlate);
-    List<Vehicle> findByUser(User user);
-
 }
