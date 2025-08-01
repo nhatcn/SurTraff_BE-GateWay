7aa2d90da03073c79cc46431a04430607a4f335a
nhatcn <149814696+nhatcn@users.noreply.github.com>
2025-08-01 15:51:55 +0700

    Nhat nm (#43)
    * Remove .idea from repository
    
    * add yaml
    
    * update up avt

--- SurTraff_BE/src/main/java/com/example/demo/controller/ViolationTypeController.java
+++ SurTraff_BE/src/main/java/com/example/demo/controller/ViolationTypeController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.repository.ViolationTypeRepository;
 import com.example.demo.service.ViolationTypesService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ViolationTypeController {
 
     @Autowired
+
     ViolationTypesService violationTypesService;
 
     @GetMapping
